3100bdb098d7b2d79b3b81a9277bb4c9ad51f054
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-05-19 18:51:08 +0300

    Social media buttons component (links not set up yet)

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const SocialButtons = () => {
     return (
         <div className="socialButtonsContainer">
-            <h1>These are the social buttons.</h1>
+            <a href="http://www.google.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook"></i></a>
+            <a href="http://www.google.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+            <a href="http://www.google.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="http://www.google.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
         </div>
     );
 }
