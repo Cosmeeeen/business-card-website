f8249b2beb44fb6d0a110cdbfd69ff1d9cb2e775
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-06-02 23:44:38 +0300

    Added new contact animation

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React , { useState } from 'react';
 
 const ProfileImage = ( props ) => {
+
+    const [contactActive, setContactActive] = useState(false);
+
     return (
         <div className="profileImageContainer">
-            <div className="profileDiv" style={{backgroundImage:`url(${props.image})`}}></div>
+            <div 
+            className={`profileDiv${contactActive?" flipped":""}`}
+            style={{backgroundImage:`url(${props.image})`}}
+            > 
+                {
+                    contactActive===true &&
+                    <div className="contactFormContainer">
+                        <h1>Hello world</h1>
+                    </div>
+                }
+                <div 
+                className="contactButtonContainer"
+                onClick={()=>{setContactActive(!contactActive)}}>
+                    <i class="fas fa-paper-plane"></i>
+                </div>
+            </div>
         </div>
     );
 }
