5807e21511cfdb38963d77ca28d8107aa8f4a0fe
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-09-04 18:32:33 +0300

    Use className instead of class where possible

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const SocialButtons = () => {
     return (
         <div className="socialButtonsContainer">
-            <a href="https://www.facebook.com/ilie.cosmos" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook"></i></a>
-            <a href="https://www.instagram.com/cosmin_ilie8/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
-            <a href="https://github.com/Cosmeeeen" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/cosmin8/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://www.facebook.com/ilie.cosmos" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/cosmin_ilie8/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+            <a href="https://github.com/Cosmeeeen" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/cosmin8/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
         </div>
     );
 }
